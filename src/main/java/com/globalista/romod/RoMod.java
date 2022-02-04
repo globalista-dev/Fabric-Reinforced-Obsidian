540d2fa92d66fc533cdd98d959802ed3f23ca4f3
Globalista <35036442+globalista-dev@users.noreply.github.com>
2022-02-04 19:26:22 -0300

    Added reinforced obsidian slabs, walls and stairs. Also fixed some errors.

--- src/main/java/com/globalista/romod/RoMod.java
+++ src/main/java/com/globalista/romod/RoMod.java
@@ -1,10 +1,12 @@
 package com.globalista.romod;
 
 import com.globalista.romod.block.ModBlocks;
-import com.globalista.romod.block.ModPaneBlocks;
+import com.globalista.romod.block.ModPaneBlock;
 import com.globalista.romod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
@@ -22,9 +24,9 @@ public class RoMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.DARK_IRON_BARS, RenderLayer.getCutout());
 		ModBlocks.callBlocks();
 		ModItems.callItems();
-		ModPaneBlocks.callPaneBlocks();
 
 		LOGGER.info("Fabric Reinforced Obsidian!");
 	}
--- src/main/java/com/globalista/romod/item/ModItems.java
+++ src/main/java/com/globalista/romod/item/ModItems.java
@@ -3,13 +3,14 @@ package com.globalista.romod.item;
 import com.globalista.romod.RoMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import static com.globalista.romod.item.ItemRegister.registerItem;
 
-public class ModItems implements ItemRegister {
+public class ModItems {
 
     public static void callItems() {
         System.out.println("Registering RoMod items");
     }
 
-    public static final Item DARK_IRON_INGOT = ItemRegister.registerItem("dark_iron_ingot",
+    public static final Item DARK_IRON_INGOT = registerItem("dark_iron_ingot",
             new Item(new FabricItemSettings().group(RoMod.ROGROUP)));
 }
